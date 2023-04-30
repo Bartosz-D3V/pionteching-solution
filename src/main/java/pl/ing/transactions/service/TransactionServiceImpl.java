0702f6d6b1f3474b4acb7fced9609b852ce0d81b
Bartosz-D3V <nix.noxi379@gmail.com>
2023-04-30 12:51:59 +0200

    chore: [transactions] Improve performance
    * Improve transactions service performance

--- src/main/java/pl/ing/transactions/service/TransactionServiceImpl.java
+++ src/main/java/pl/ing/transactions/service/TransactionServiceImpl.java
@@ -1,7 +1,6 @@
 package pl.ing.transactions.service;
 
 import jakarta.inject.Singleton;
-import java.math.RoundingMode;
 import java.util.Collection;
 import java.util.HashMap;
 import pl.ing.transactions.domain.Account;
@@ -12,30 +11,29 @@ public class TransactionServiceImpl implements TransactionService {
     @Override
     public Collection<Account> processTransactions(Collection<Transaction> transactions) {
         var map = new HashMap<String, Account>();
-        for (Transaction transaction : transactions) {
+        transactions.forEach(transaction -> {
             var creditAccount = transaction.creditAccount();
             var debitAccount = transaction.debitAccount();
             var amount = transaction.amount();
 
-            map.compute(
-                    creditAccount,
-                    (key, value) -> (value == null)
-                            ? new Account(creditAccount, 0, 1, amount.setScale(2, RoundingMode.HALF_UP))
-                            : new Account(
-                                    creditAccount,
-                                    value.debitCount(),
-                                    value.creditCount() + 1,
-                                    value.balance().add(amount).setScale(2, RoundingMode.HALF_UP)));
-            map.compute(
-                    debitAccount,
-                    (key, value) -> (value == null)
-                            ? new Account(debitAccount, 1, 0, amount.negate().setScale(2, RoundingMode.HALF_UP))
-                            : new Account(
-                                    debitAccount,
-                                    value.debitCount() + 1,
-                                    value.creditCount(),
-                                    value.balance().subtract(amount).setScale(2, RoundingMode.HALF_UP)));
-        }
+            map.compute(creditAccount, (key, value) -> {
+                if (value == null) {
+                    return new Account(creditAccount, 0, 1, amount);
+                }
+                value.increaseCreditCount();
+                value.addBalance(amount);
+                return value;
+            });
+
+            map.compute(debitAccount, (key, value) -> {
+                if (value == null) {
+                    return new Account(debitAccount, 1, 0, amount.negate());
+                }
+                value.increaseDebitCount();
+                value.subtractBalance(amount);
+                return value;
+            });
+        });
 
         return map.values().stream().sorted().toList();
     }
